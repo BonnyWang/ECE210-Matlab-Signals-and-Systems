49172d1d5e891e5a1c97caa273c2cd889d1bdfa5
BW <bonny.y.gardy@gmail.com>
2020-04-18 06:48:11 -0400

    Delete myFunction.m

--- myFunction.m
+++
@@ -1,9 +0,0 @@
-function b = myFunction(a) % main function
-   b = squareMe(a)+doubleMe(a);
-end
-function y = squareMe(x) % local functions; can only be referenced by functions in the same file
-   y = x.^2;
-end
-function y = doubleMe(x)
-   y = x.*2;
-end
